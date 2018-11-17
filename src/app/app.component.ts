a03cd1e4e1c9954b0c8912b586e0922c5be9a1f1
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-11-17 00:23:47 -0600

    Adding changes for embedding Tableau report

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// Declare var tableau because it is referencing the tableau js library
+declare var tableau: any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tableau-in-angular';
+  title = 'Tableau in Angular';
+  // Declare an instance of viz
+  viz: any;
+
+  ngOnInit() {
+    var placeholderDiv = document.getElementById('vizContainer');
+    // Replace this url with the url of your Tableau dashboard
+    var url = 'https://public.tableau.com/views/All_Locker_Reports_0/Dashboard1?:embed=y&:display_count=yes';
+    var options = {
+      hideTabs: true,
+      width: "80%",
+      height: "500px",
+      onFirstInteractive: function() {
+        // The viz is now ready and can be safely used.
+        console.log("Run this code when the viz has finished loading.");
+      }
+    };
+    // Creating a viz object and embed it in the container div.
+    this.viz = new tableau.Viz(placeholderDiv, url, options);
+  }
 }
